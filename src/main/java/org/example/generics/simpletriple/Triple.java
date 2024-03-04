4da69714f332ab21d49ec44344e4464916b55142
Владислав <megoboom17@gmail.com>
2024-03-04 14:49:55 +0300

    Java: Дженерики 4. реализация Дженериков

---
+++ src/main/java/org/example/generics/simpletriple/Triple.java
@@ -0,0 +1,12 @@
+package org.example.generics.simpletriple;
+
+public interface Triple<L, M, R> {
+
+    L getLeft();
+    M getMiddle();
+    R getRight();
+
+    void setLeft(L left);
+    void setMiddle(M middle);
+    void setRight(R right);
+}
