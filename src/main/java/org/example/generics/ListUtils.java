082b4638ce1de9fffe165282b6750945788fafb3
Владислав <megoboom17@gmail.com>
2024-03-05 18:06:29 +0300

    Java Дженерики 7 Статичные методы

--- src/main/java/org/example/generics/ListUtils.java
+++ src/main/java/org/example/generics/ListUtils.java
@@ -2,11 +2,16 @@ package org.example.generics;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 //Java Дженерики 5 Параметрический полиморфизм
 // Создайте параметризованный класс ListUtils<T>. В классе создайте метод merge(), который принимает два
 // параметризованных списка List и сливает их. Метод должен вернуть новый параметризованный список, который представляет
 // собой слияние двух переданных списков.
+
+//В классе ListUtils создайте обобщенный статический метод filter, который фильтрует коллекцию. Метод принимает список
+// элементов и лямбда-функцию Predicate, которая содержит логику проверки элемента. Метод должен вернуть новый список,
+// в котором отсутствуют элементы, не удовлетворяющие условию
 public class ListUtils<T> {
 
     public  List<T> merge(List<T> list1, List<T> list2) {
@@ -17,16 +22,26 @@ public class ListUtils<T> {
         return mergedList;
     }
 
+    public static <T> List<T> filter(List<T> listToFilter, Predicate<T> func) {
+        List<T> filteredList = new ArrayList<>();
+
+        for (var item : listToFilter) {
+            if (func.test(item)) {
+                filteredList.add(item);
+            }
+        }
+        return filteredList;
+    }
+
     public static void main(String[] args) {
 
-        var util = new ListUtils<Integer>();
+        var languages = new ArrayList<>(List.of("java", "javascript", "php", "ruby"));
+        var result = ListUtils.filter(languages, l -> l.startsWith("j"));
+        System.out.println(result); // => ["java", "javascript"]
 
-        var coll1 = new ArrayList<>(List.of(1, 2, 3));
-        var coll2 = new ArrayList<>(List.of(3, 4, 5));
-
-        var result = util.merge(coll1, coll2);
-        System.out.println(result); // [1, 2, 3, 3, 4, 5]
-
+        var numbers = new ArrayList<>(List.of(1, 2, 5, 0, 8, 3));
+        var result1 = ListUtils.filter(numbers, n -> n > 4);
+        System.out.println(result1); // => [5,
 
     }
 
